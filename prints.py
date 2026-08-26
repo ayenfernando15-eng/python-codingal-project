2a4ba7eb3dcad4280ee53671e5752af760382832
ayenfernando15-eng <ayenfernando15@gmail.com>
2026-08-26 18:39:46 +1200

    keywords and variables

---
+++ prints.py
@@ -0,0 +1,3 @@
+print("hello", 1236)
+print("My name is Ayen \n I am in year 7 ")
+print("My hobbies are sleeping and drawing" ,end=".")
---
+++ variables.py
@@ -0,0 +1,8 @@
+name = "Ayen"
+age = 11
+print(name)
+print(age)
+
+school = input ("Enter the name of your school : ")
+print( school)
+print("My school name is:  " , school)
