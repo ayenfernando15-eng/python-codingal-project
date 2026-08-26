4ea551728a28ee98d889872a0614aac393213384
ayenfernando15-eng <ayenfernando15@gmail.com>
2026-08-26 18:04:24 +1200

    home work

---
+++ daily routine.py
@@ -0,0 +1,27 @@
+print("I wake up at 7:15 in the morning to get ready for school")
+# this is a comment
+print("I start to eat my breakfast at 7:40")
+# this is a comment
+print(" then I go to school and arive at 7:35")
+# this is a comment
+print("the start at 7:45 and begin with maths follwed by reading.")
+# this is a comment
+print("then we have our interval at 10:55.then I eat my snack saving my other food for lunch.")
+# this is a comment
+print("then we do our writing till lunch.")
+# this is a comment
+print("at luch i play man hunt with my friends.")
+# this is a comment
+print("then we do are topic we learn about human migration.") 
+# this is a comment
+print("then we leave school at 3:00 and I arrive at home at 3:40.")
+# this is a comment
+print("then I go have a was and eat my lunch.")
+# this is a comment
+print("Then i do my school home work or draw and do art and play with my mother>")
+# this is a comment
+print("then I got to my room and changed in to my pyjamas.")
+# this is a comment
+print("after that, my mother would make my dinner for me and my brother to eat.")
+# this is a comment
+print("then I would go to bed at 9:00 so I can wake up for the next day of school")
