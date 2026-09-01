29570f0e25f6bb8ce1a58b3eca52fe08d0a45133
ayenfernando15-eng <ayenfernando15@gmail.com>
2026-09-01 20:48:33 +1200

    hw

---
+++ personal_goals.py
@@ -0,0 +1,10 @@
+goal = "Finish my Ancient Greece speech"
+
+print("My goal is:", goal)
+
+finished = input("Have I finished my Greece speech? (yes/no): ")
+
+if finished == "yes":
+    print("Goal completed! Great job!")
+else:
+    print("Keep working on your speech!")
