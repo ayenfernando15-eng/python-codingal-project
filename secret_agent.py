dca6b553c8362add755af8f494e9d88a4d69706b
ayenfernando15-eng <ayenfernando15@gmail.com>
2026-09-02 18:46:32 +1200

    secret_agent.py

---
+++ secret_agent.py
@@ -0,0 +1,14 @@
+name=input("enter the your real name agent : ")
+gadget=input("enter your favorite gadget to use : ")
+
+agent_number = 1000000000000000000000000000000000
+speed_rating = 100000000000000
+mission_count = 1
+height_m = 0.54
+is_active = True
+
+print("name : " ,name, "->type", type(name))
+print("gadget : " ,gadget,"->type", type(gadget))
+print("agent number",agent_number,"->type", type(agent_number))
+print("speed rating" ,speed_rating,"->type", type(speed_rating))
+
